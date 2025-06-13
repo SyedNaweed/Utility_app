7504d5c3db1ffc2d5ca9ce9caec520ea1ae17b65
SyedNaweed <naweedu2003@gmail.com>
2025-06-13 11:20:55 +0530

    server 3

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -27,7 +27,7 @@ router.post('/login', async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'Invalid Password' });
 
-        // Generate token
+        // Generate token       
         const token = jwt.sign({ id: user._id }, 'secretkey', { expiresIn: '1h' });
         res.json({ message: 'Login successful', token });
 
